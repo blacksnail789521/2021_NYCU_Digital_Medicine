bb35c37e552015e44104acf782c79b170466e91c
tommytyc <tommytyc.cs06@nctu.edu.tw>
2021-11-14 14:58:54 +0800

    use png and fix some bugs

--- case2/utils.py
+++ case2/utils.py
@@ -2,23 +2,37 @@ from pydicom import dcmread
 import os
 import pandas as pd
 import numpy as np
-import pickle
+import cv2
+from PIL import Image
+from tqdm import tqdm
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 
 Data_dir = './Data/'
 
+def preporcess(img):
+    img = img.astype(np.float32)
+    img = (img / img.max()) * 255
+    return img
+
 def readout_dataset(dir, label_path):
     train_path, valid_path = {}, {}
     train_df = pd.read_csv(label_path)
     valid_df = pd.DataFrame(columns=['FileID', 'path'])
-    for root, _, files in os.walk(os.path.join(dir, 'train/')):
+    for root, _, files in tqdm(os.walk(os.path.join(dir, 'train/'))):
         for file in files:
-            train_path[file.split('.dcm')[0]] = os.path.join(root, file)
-    for root, _, files in os.walk(os.path.join(dir, 'valid/')):
+            with open(os.path.join(root, file), 'rb') as f:
+                ds = dcmread(f).pixel_array
+            ds = preporcess(ds)
+            cv2.imwrite(os.path.join(root, file).replace('dcm', 'png'), ds)
+            train_path[file.split('.dcm')[0]] = os.path.join(root, file).replace('dcm', 'png')
+    for root, _, files in tqdm(os.walk(os.path.join(dir, 'valid/'))):
         for file in files:
-            valid_path[file.split('.dcm')[0]] = os.path.join(root, file)
+            with open(os.path.join(root, file), 'rb') as f:
+                ds = dcmread(f).pixel_array
+            cv2.imwrite(os.path.join(root, file).replace('dcm', 'png'), ds)
+            valid_path[file.split('.dcm')[0]] = os.path.join(root, file).replace('dcm', 'png')
     
     train_df['label'] = ((train_df.iloc[:, 1:] == 1).idxmax(1)).astype('category').cat.codes
     y = [train_path[x] for x in train_df['FileID'].tolist()]
@@ -47,14 +61,11 @@ class Covid_Dataset(Dataset):
         self.mode = mode
 
     def __len__(self):
-        return len(self.data)
+        return len(self.label)
 
     def __getitem__(self, index):
         row = self.label.iloc[index]
-        with open(row['path'], 'rb') as f:
-            img = dcmread(f).pixel_array
-        img = np.array(img, dtype=np.float32)[np.newaxis]
-        img = torch.from_numpy(img)
+        img = Image.open(row['path'])
         img = self.preprocess(img)
         if self.mode == 'train':
             img = self.transform(img)
